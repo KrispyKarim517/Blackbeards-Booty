d55993be138366b1a38e794d91311292c23554de
something999 <wongny1@uci.edu>
2020-08-03 20:58:40 -0700

    Added version 1 of the UI: Drop-down menus.

---
+++ eJam2/eJamGambling/Assets/Scripts/UIScripts/script_DetectDropDown.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+    AUTHOR: Nichole Wong
+    UNITY VERSION: 2020.1.0f1
+    LAST MODIFIED: 8/3/2020
+    
+    This script detects whether the drop down menu has been activated
+    and performs an effect.
+*/
+public class script_DetectDropDown : MonoBehaviour
+{
+    [SerializeField] private Transform transform_SlotTransform = null;
+    [SerializeField] private Transform transform_Arrow = null;
+    private bool bool_MenuActivated = false;
+    
+    private void Start()
+    {
+        
+    }
+    
+    /*
+        When the drop-down menu is activated, Unity adds a child to the 
+        drop-down object (the child representing the drop-down list).
+        To detect whether the drop down menu has been activated, the Update 
+        function will check how many children are attached to the menu.
+    */
+    private void Update()
+    {
+        if (!bool_MenuActivated && transform_SlotTransform.childCount > 3)
+        {
+            transform_Arrow.rotation *= Quaternion.Euler(0f, 0f, 180f);
+            bool_MenuActivated = true;
+        }
+    }
+    
+    public void FlipArrow()
+    {
+        transform_Arrow.rotation *= Quaternion.Euler(0f, 0f, 180f * 0f);
+        bool_MenuActivated = false;
+    }
+}
---
+++ eJam2/eJamGambling/Assets/Scripts/UIScripts/script_InputFieldSettings.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+    AUTHOR: Nichole Wong
+    UNITY VERSION: 2020.1.0f1
+    LAST MODIFIED: 8/3/2020
+    
+    This script controls the following settings for the UI input field:
+        - Input character limit
+        - Input confirmation button behavior
+*/
+public class script_InputFieldSettings : MonoBehaviour
+{
+    [SerializeField] private InputField inputfield_InputBox = null;
+    /*
+        We chose to enforce a character limit for the following reasons:
+            - Helps prevent trolling (by people who want to flood the screen with 
+               overly long player names)
+            - Encourages people to choose shorter, memorable names. 
+    
+        When the InputField's width is set to 600 characters, only 15 characters 
+        can fit within the box.
+    */
+    [SerializeField][MinAttribute(0)] private int int_NameCharacterLimit = 0;
+    [SerializeField] private Button button_confirmationButton = null;
+
+    // Set the name character limit to the desired setting.
+    private void Start()
+    {
+            inputfield_InputBox.characterLimit = int_NameCharacterLimit;
+            
+    }
+    
+    /*
+       The Update() function is used to detect whether the player has 
+       entered in a name. If yes, allow the player to continue. 
+    */
+    private void Update()
+    {
+        if (inputfield_InputBox.text.Length == 0) button_confirmationButton.interactable = false;
+        else button_confirmationButton.interactable = true;
+    }
+}
