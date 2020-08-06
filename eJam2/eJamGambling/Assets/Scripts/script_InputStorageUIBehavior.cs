97ab57baece1e5cad4dee1c095ce6c9e240d111c
rygreen <rygreen99@gmail.com>
2020-08-06 02:15:25 -0700

    ui behavior for storage script
    :^)

---
+++ eJam2/eJamGambling/Assets/Scripts/script_InputStorageUIBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_InputStorageUIBehavior : MonoBehaviour
+{
+    public script_InputStorage storage;
+    public Text nameField;
+
+    Color[] currCode;
+    int currIndex;
+
+    void Start()
+    {
+        ClearCode();
+    }
+
+    //Adds given color to the code
+    public void AddColor(Color temp_color)
+    {
+        currCode[currIndex] = temp_color;
+        ++currIndex;
+    }
+
+    public void ClearCode()
+    {
+        currCode = new Color[5];
+        currIndex = 0;
+    }
+
+    public void EnterCode()
+    {
+        Color[] tempCode = new Color[currIndex];
+        for(int count = 0; count < currIndex; ++count)
+        {
+            tempCode[0] = currCode[0];
+        }
+
+        storage.AddInput(nameField.text, tempCode);
+        ClearCode();
+    }
+}
