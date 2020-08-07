b7417c7dfb65511f2f8c4a0c5c0afad8978981dd
rygreen <rygreen99@gmail.com>
2020-08-07 14:51:52 -0700

    Add Code Storage
    :^)

---
+++ eJam2/eJamGambling/Assets/Scripts/script_CodeStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_CodeStorage : MonoBehaviour
+{
+    public int StorageSize = 3;
+    List<Color[]> codes = new List<Color[]>();
+
+    public void AddCode(Color[] newCode)
+    {
+        if(codes.Count == StorageSize)
+        {
+            codes.RemoveAt(0);
+        }
+        codes.Add(newCode);
+    }
+
+    public List<Color[]> GetCodes()
+    {
+        return codes;
+    }
+}
