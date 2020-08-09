7995504271289821a4bb4cd87b8800141264fe1c
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2020-08-08 21:09:55 -0700

    Scripte Edits

---
+++ eJam2/eJamGambling/Assets/Scripts/script_GemSpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_GemSpawn : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer slot1;
+    [SerializeField] SpriteRenderer slot2;
+    [SerializeField] SpriteRenderer slot3;
+    [SerializeField] SpriteRenderer slot4;
+    [SerializeField] SpriteRenderer slot5;
+
+    SpriteRenderer[] slotArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slotArray = new SpriteRenderer[]
+            {
+                slot1,
+                slot2,
+                slot3,
+                slot4,
+                slot5
+            };
+    }
+
+    public void SpriteArray(Sprite[] arrSprites)
+    {
+        for (int i = 0; i < slotArray.Length; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
